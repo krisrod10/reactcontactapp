524fffceb408adcfdb04e9e40f75917ca0122624
krisrod10 <82917336+krisrod10@users.noreply.github.com>
2022-02-08 16:31:56 -0600

    test

---
+++ src/Router.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
---
+++ src/helpers.js
@@ -0,0 +1,20 @@
+import cookie from 'cookie';
+
+export function CapitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function checkAuth(){
+    const cookies = cookie.parse(document.cookie);
+    return cookies['loggedIn'] ? true : false;
+}
+
+export function getToken(){
+    const cookies = cookie.parse(document.cookie);
+    return cookies['token'];
+}
+
+export function getUserId(){
+    const cookies = cookie.parse(document.cookie);
+    return cookies['userId'];
+}
